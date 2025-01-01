2121889d158e6ae0346f928390c8f1136d6fea40
XxCodeStarkxX <semperfitrios@gmail.com>
2024-12-31 20:56:26 -0600

    added charAt and slice methods of a starting

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    //charAt method
+//const word = "freecodecamp"
+//const firstLetter = word.charAt(0);
+//console.log(firstLetter);
+
+    //slice-slices of characters of the string
+
+ const word = "freecodecamp"
+ const remainingLetters = word.substring(1);
+ console.log(remainingLetters);
+
+ 
