858164f09c89309f9d9c15e1f610b031712c4559
XxCodeStarkxX <semperfitrios@gmail.com>
2025-01-01 18:24:12 -0600

    this is how to cap the first letter in a strng and make sure the rest of the leters are lowercase

--- script.js
+++ script.js
@@ -6,8 +6,31 @@
 
     //slice-slices of characters of the string
 
- const word = "freecodecamp"
- const remainingLetters = word.substring(1);
- console.log(remainingLetters);
+ //const word = "freecodecamp"
+ // remainingLetters = word.substring(1);
+ ////.log(remainingLetters);
 
- 
+    //to uppercase
+
+ //const firstLetter = "freedom"
+ //onst firstLetterCap = firstLetter.toLocaleUpperCase();
+//console.log(firstLetterCap);
+
+    //all strings combined to capitalize the first letter in a string.
+/*
+    const word = "freecOdeCamP"
+    const capitalize = 
+        word.charAt(0).toUpperCase() + word.slice(1).toLocaleLowerCase();
+
+    console.log(capitalize);    
+
+ */
+
+    function capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    
+    }
+    console.log(capitalize("freeDom"));
+
+
+    
